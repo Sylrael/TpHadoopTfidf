01afbe16e0973d93e95de512836218a79aa54690
Sylrael <stephane.tiv@gmail.com>
2018-03-03 18:10:42 +0100

    Initial commit

---
+++ src/main/java/org/formation/hadoop/tfidf/App.java
@@ -0,0 +1,13 @@
+package org.formation.hadoop.tfidf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
